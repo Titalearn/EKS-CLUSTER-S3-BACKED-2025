a8f14c51c1c4c57a419e341bf5448fab0ec4d9ee
Titalearn <126238347+Titalearn@users.noreply.github.com>
2024-08-01 17:11:12 +0200

    Update users-managers.tf

--- Admin-eks/users-managers.tf
+++ Admin-eks/users-managers.tf
@@ -27,8 +27,8 @@ resource "aws_iam_group_policy" "masters_policy" {
 
 resource "aws_iam_group_membership" "masters_team" {
   name  = "masters-group-membership"
-  users = [aws_iam_user.eks_user[0].name]
-  group = aws_iam_group.eks_developer.name
+  users = [aws_iam_user.eks_user[1].name, aws_iam_user.eks_user[2].name, aws_iam_user.eks_user[3].name]
+  group = aws_iam_group.eks_masters.name
 }
 
 resource "aws_iam_account_password_policy" "strict" {
@@ -40,17 +40,17 @@ resource "aws_iam_account_password_policy" "strict" {
   allow_users_to_change_password = true
 }
 
-resource "aws_iam_role" "managers" {
-  name               = "Manager-eks-Role"
-  assume_role_policy = data.aws_iam_policy_document.manager_assume_role.json
+resource "aws_iam_role" "masters" {
+  name               = "Masters-eks-Role"
+  assume_role_policy = data.aws_iam_policy_document.masters_assume_role.json
 }
 
 
 resource "aws_iam_role_policy_attachment" "admin_policy" {
-  role       = aws_iam_role.managers.name
+  role       = aws_iam_role.masters.name
   policy_arn = aws_iam_policy.eks_admin.arn
 }
 
 resource "aws_iam_policy" "eks_admin" {
   name   = "eks-admin"
-  policy = data.aws_iam_policy_document.admin.json
+  policy = data.aws_iam_policy_document.masters.json
