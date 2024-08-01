88d0b48ff5b9f5410c1c87ec8ecde12df4b623c3
Titalearn <126238347+Titalearn@users.noreply.github.com>
2024-08-01 17:21:21 +0200

    Update data-manager.tf

--- Admin-eks/data-manager.tf
+++ Admin-eks/data-manager.tf
@@ -1,1 +1,54 @@
 
+data "aws_iam_policy_document" "masters" {
+  statement {
+    sid       = "AllowAdmin"
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+  statement {
+    sid    = "AllowPassRole"
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "iam:PassedToService"
+      values   = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "masters_assume_role" {
+  statement {
+    sid    = "AllowAccountAssumeRole"
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+      #identifiers = ["data.aws_caller_identity.current.account_id]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "masters_role" {
+  statement {
+    sid    = "AllowMastersAssumeRole"
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [data.aws_caller_identity.current.account_id]
+      #identifiers = ["data.aws_caller_identity.current.account_id]
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
