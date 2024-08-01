eaa8383e9297d4d620002a0df962c303af3060b1
Titalearn <126238347+Titalearn@users.noreply.github.com>
2024-08-01 17:06:18 +0200

    Update variables.tf

--- Admin-eks/variables.tf
+++ Admin-eks/variables.tf
@@ -1,1 +1,17 @@
 
+variable "username" {
+  type    = list(any)
+  default = ["mark", "manager","jane"]
+}
+
+variable "env" {
+  type    = list(any)
+  default = ["Development", "Production"]
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    Env = "Production"
+  }
+}
